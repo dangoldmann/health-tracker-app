c8ca026ea7f64d47248938779a37c975f832f13d
Dan Goldman <goldmandan8@gmail.com>
2026-05-08 12:31:32 -0300

    Remove initial health module scaffold

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { HealthModule } from './health/health.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       ignoreEnvFile: false,
     }),
-    HealthModule,
   ],
 })
 export class AppModule {}
